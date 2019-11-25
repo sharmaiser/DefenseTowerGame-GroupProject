9c9ab37bdb94bb606446c474770962ad81c2a945
sharmaiser <sharmaiser@users.noreply.github.com>
2019-11-25 16:16:40 -0500

    Chapter 18 WaypointManager
    *WaypointManager Added

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/WayPoint/WayPointManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class WayPointManager : MonoBehaviour
+{
+    //1
+    public static WayPointManager Instance;
+    //2
+    public List<Path> Paths = new List<Path>();
+    void Awake()
+    {
+        //3
+        Instance = this;
+    }
+    //4
+    public Vector3 GetSpawnPosition(int pathIndex)
+    {
+        return Paths[pathIndex].WayPoints[0].position;
+    }
+}
+//5
+[System.Serializable]
+public class Path
+{
+    public List<Transform> WayPoints = new List<Transform>();
+}
