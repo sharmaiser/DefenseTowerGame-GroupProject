24cecc95207b8f8e504baf47279bfa0f8466a59a
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-11-25 16:16:05 -0500

    made enemy,looprotate, autoscaler, and Utilitymethods scripts

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/AutoScaler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScaler : MonoBehaviour
+{
+    public float scaleSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += (new Vector3(scaleSpeed, scaleSpeed, scaleSpeed) * Time.deltaTime);
+    }
+}
--- starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/UtilityMethods.cs
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/UtilityMethods.cs
@@ -28,7 +28,7 @@
  * THE SOFTWARE.
  */
 
-using UnityEngine;
+ using UnityEngine;
 
 public static class UtilityMethods
 {
@@ -36,7 +36,23 @@ public static class UtilityMethods
     {
         Vector2 screenPoint = Camera.main.WorldToScreenPoint(worldPosition);
         rectTransform.position = screenPoint;
+    }
 
+    // 1 
+    public static Quaternion SmoothlyLook(Transform fromTransform, Vector3 toVector3)
+    {
 
+        //2  
+        if (fromTransform.position == toVector3)
+        {
+            return fromTransform.localRotation;
+        }
+
+        //3 
+        Quaternion currentRotation = fromTransform.localRotation;
+        Quaternion targetRotation = Quaternion.LookRotation(toVector3 - fromTransform.position);
+
+        //4 
+        return Quaternion.Slerp(currentRotation, targetRotation, Time.deltaTime * 10f);
     }
 }
