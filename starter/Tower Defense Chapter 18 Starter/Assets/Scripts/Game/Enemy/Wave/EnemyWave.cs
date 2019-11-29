aa4f1e3579569d6a4cfd38d25f21d1f20890f891
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-11-28 23:00:01 -0500

    made a wave folder and put an emeny waves script into it and a WaveManager script

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Wave/EnemyWave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//1 
+[Serializable] public class EnemyWave
+{
+    //2  
+    public int pathIndex;
+    //3  
+    public float startSpawnTimeInSeconds;
+    //4  
+    public float timeBetweenSpawnsInSeconds = 1f;
+    //5   
+    public List<GameObject> listOfEnemies = new List<GameObject>();
+}
