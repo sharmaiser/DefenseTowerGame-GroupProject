7b1572d35f6b14c121dcadeabbc7f9bfd06816e4
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-12-02 16:02:33 -0500

    Updated tower script by adding public void ShowTowerInfo() after LevelUp()

--- starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/Tower.cs
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/Tower.cs
@@ -114,4 +114,9 @@ public class Tower : MonoBehaviour
         timeBetweenAttacksInSeconds *= 0.7f;
         aggroRadius *= 1.20f;
     }
+
+    public void ShowTowerInfo()
+    {
+        UIManager.Instance.ShowTowerInfoWindow(this);
+    }
 }
