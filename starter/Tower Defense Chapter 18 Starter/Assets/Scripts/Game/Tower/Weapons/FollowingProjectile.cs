cf02cdf7fdb6e4f095ee134543878d868dbc812e
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-12-02 15:47:46 -0500

    projectiles

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/Weapons/FollowingProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FollowingProjectile : MonoBehaviour
+{
+    public Enemy enemyToFollow;
+
+    public float moveSpeed = 15f;
+
+    private void Update()
+    {
+        if (enemyToFollow == null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.LookAt(enemyToFollow.transform);
+            GetComponent<Rigidbody>().velocity = transform.forward * moveSpeed;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Enemy>() == enemyToFollow)
+        {
+            OnHitEnemy();
+        }
+    }
+
+    protected abstract void OnHitEnemy();
+}
---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/Weapons/Stone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : FollowingProjectile
+{
+    public float damage;
+
+    protected override void OnHitEnemy()
+    {
+        enemyToFollow.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
