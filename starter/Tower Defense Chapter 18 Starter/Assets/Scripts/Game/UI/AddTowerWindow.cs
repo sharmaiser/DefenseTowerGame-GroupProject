f49dbd94ba85bc92902a5b87fba6cc3bebd01483
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-12-02 15:36:35 -0500

    made towermanager script, and an addtower script

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/UI/AddTowerWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AddTowerWindow : MonoBehaviour
+{
+    public GameObject towerSlotToAddTowerTo;
+
+    public void AddTower(string towerTypeAsString)
+    {
+        TowerType type = (TowerType)Enum.Parse(typeof(TowerType),                   
+            towerTypeAsString, true);
+
+        if (TowerManager.Instance.GetTowerPrice(type) <=     
+            GameManager.Instance.gold)
+        {
+            GameManager.Instance.gold -= TowerManager.Instance   
+                .GetTowerPrice(type); 
+                TowerManager.Instance.CreateNewTower(towerSlotToAddTowerTo, type);
+            gameObject.SetActive(false);
+        }
+    } 
+}
