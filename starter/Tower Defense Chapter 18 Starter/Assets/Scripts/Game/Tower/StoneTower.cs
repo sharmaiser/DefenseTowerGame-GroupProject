8d3a90c99336c964d9a127514c991bae7e48ba3b
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-12-02 15:16:10 -0500

    stonetower script

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Tower/StoneTower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneTower : Tower
+{
+    public GameObject stonePrefab;
+
+    protected override void AttackEnemy()
+    {
+        base.AttackEnemy();
+        GameObject stone = (GameObject)Instantiate(stonePrefab, towerPieceToAim.position, Quaternion.identity);
+    }
+}
