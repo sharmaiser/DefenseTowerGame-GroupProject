8befc40adf43a5469adb2f4b09537c8631a8be23
sharmaiser <sharmaiser@users.noreply.github.com>
2019-12-04 21:00:27 -0500

    UIManager & WaveManger Updated
    *Script for Center Window added.
    *Bugs for UIManger Fixed.

--- starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Wave/WaveManager.cs
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Wave/WaveManager.cs
@@ -4,26 +4,19 @@ using System.Collections.Generic;
 
 public class WaveManager : MonoBehaviour
 {
-    //1 
     public static WaveManager Instance;
-    //2
     public List<EnemyWave> enemyWaves = new List<EnemyWave>();
-    //3
     private float elapsedTime = 0f;
-    //4 
     private EnemyWave activeWave;
-    //5
     private float spawnCounter = 0f;
-    //6
     private List<EnemyWave> activatedWaves = new List<EnemyWave>();
 
-    //1 
+
     void Awake()
     {
         Instance = this;
     }
     
-    //2 
     void Update()
     {
          elapsedTime += Time.deltaTime; 
@@ -41,6 +34,8 @@ public class WaveManager : MonoBehaviour
                 activatedWaves.Add(enemyWave);
                 spawnCounter = 0f;
                 GameManager.Instance.waveNumber++;
+                UIManager.Instance.ShowCenterWindow("Wave " + GameManager.
+                Instance.waveNumber);
                 break;
             }
         }
--- starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/UI/UIManager.cs
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/UI/UIManager.cs
@@ -16,53 +16,73 @@ public class UIManager : MonoBehaviour
     public Text txtEscapedEnemies;
     public Transform enemyHealthBars;
     public GameObject enemyHealthBarPrefab;
+    public GameObject centerWindow;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    private void UpdateTopBar()
+    {
+        txtGold.text = GameManager.Instance.gold.ToString();
+        txtWave.text = "Wave " + GameManager.Instance.waveNumber + " / " + WaveManager.Instance.enemyWaves.Count;
+        txtEscapedEnemies.text = "Escaped Enemies " + GameManager.Instance.escapedEnemies + " / " + GameManager.Instance.maxAllowedEscapedEnemies;
+    }
+
+    public void ShowAddTowerWindow(GameObject towerSlot)
+    {
+        addTowerWindow.SetActive(true); addTowerWindow.GetComponent<AddTowerWindow>().towerSlotToAddTowerTo = towerSlot;
+        UtilityMethods.MoveUiElementToWorldPosition(addTowerWindow.GetComponent<RectTransform>(), towerSlot.transform.position);
+    }
+
+    public void ShowWinScreen()
+    {
+        blackBackground.SetActive(true);
+        winGameWindow.SetActive(true);
+    }
+
+    public void ShowLoseScreen()
+    {
+        blackBackground.SetActive(true);
+        loseGameWindow.SetActive(true);
+    }
+
+    public void CreateHealthBarForEnemy(Enemy enemy)
+    {
+        GameObject healthBar = Instantiate(enemyHealthBarPrefab);
+        healthBar.transform.SetParent(enemyHealthBars, false);
+        healthBar.GetComponent<EnemyHealthBar>().enemy = enemy;
+    }
+
+    private IEnumerator EnableAndDisableCenterWindow()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(.4f);
+            centerWindow.SetActive(true);
+            yield return new WaitForSeconds(.4f);
+            centerWindow.SetActive(false);
+        }
+    }
+
+    public void ShowCenterWindow(string text)
+    {
+        centerWindow.transform.FindChild("TxtWave").GetComponent<Text>().
+        text = text;
+        StartCoroutine(EnableAndDisableCenterWindow());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTopBar();
+    }
+
+    public void ShowTowerInfoWindow(Tower tower)
+    {
+        towerInfoWindow.GetComponent<TowerInfoWindow>().tower = tower; towerInfoWindow.SetActive(true);
+        UtilityMethods.MoveUiElementToWorldPosition(towerInfoWindow.GetComponent<RectTransform>(), tower.transform.position);
+    }
 }
 
-void Awake()
-{
-    Instance = this;
-}
-
-private void UpdateTopBar()
-{
-    txtGold.text = GameManager.Instance.gold.ToString();
-    txtWave.text = "Wave " + GameManager.Instance.waveNumber + " / " +     WaveManager.Instance.enemyWaves.Count;
-    txtEscapedEnemies.text = "Escaped Enemies " +     GameManager.Instance.escapedEnemies + " / " +     GameManager.Instance.maxAllowedEscapedEnemies;
-}
-
-public void ShowAddTowerWindow(GameObject towerSlot)
-{
-    addTowerWindow.SetActive(true);   addTowerWindow.GetComponent<AddTowerWindow>().     towerSlotToAddTowerTo = towerSlot;
-    UtilityMethods.MoveUiElementToWorldPosition(addTowerWindow.GetComponent<RectTransform>(), towerSlot.transform.position);
-}
-
-public void ShowWinScreen()
-{
-    blackBackground.SetActive(true);
-    winGameWindow.SetActive(true);
-}
-
-public void ShowLoseScreen()
-{
-    blackBackground.SetActive(true);
-    loseGameWindow.SetActive(true);
-}
-
-public void CreateHealthBarForEnemy(Enemy enemy)
-{
-    GameObject healthBar = Instantiate(enemyHealthBarPrefab);
-    healthBar.transform.SetParent(enemyHealthBars, false);
-    healthBar.GetComponent<EnemyHealthBar>().enemy = enemy;
-}
-
-// Update is called once per frame
-void Update()
-{
-    UpdateTopBar();
-}
-
-public void ShowTowerInfoWindow(Tower tower)
-{
-    towerInfoWindow.GetComponent<TowerInfoWindow>().tower = tower; towerInfoWindow.SetActive(true);
-    UtilityMethods.MoveUiElementToWorldPosition(towerInfoWindow.GetComponent<RectTransform>(), tower.transform.position);
-}
